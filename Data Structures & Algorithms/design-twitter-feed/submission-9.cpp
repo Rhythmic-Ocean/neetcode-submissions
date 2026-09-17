d38a652c6cbca2772ea870c0ff51eb027501dcdd
Samir Khanal <96647127+Rhythmic-Ocean@users.noreply.github.com>
2026-09-17 17:43:10 -0500

    Add: design-twitter-feed - submission-9

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-9.cpp
@@ -0,0 +1,54 @@
+class Twitter {
+        unordered_map<int, unordered_set<int>> follower_map {};
+        unordered_map<int, std::deque<pair<int, int>>> tweet_map {};
+        int count {};
+public:
+    Twitter() {
+    }
+    
+    void postTweet(int userId, int tweetId) {
+       if(tweet_map[userId].size() >= 10) tweet_map[userId].pop_front();
+       tweet_map[userId].emplace_back(make_pair(tweetId, count));
+       count++; 
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+       vector<int> res {};
+        if(!follower_map[userId].contains(userId))
+            follower_map[userId].insert(userId);
+
+       priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap {};
+       priority_queue<vector<int>> maxHeap {};
+       for(auto followee: follower_map[userId]){
+            if(tweet_map[followee].empty()) continue;
+            int indx = tweet_map[followee].size() - 1;
+            auto latest_tweet = tweet_map[followee][indx];
+            minHeap.emplace(std::vector{latest_tweet.second, latest_tweet.first, followee, indx});
+            if(minHeap.size() > 10) minHeap.pop();
+       } 
+       while(!minHeap.empty()){
+        maxHeap.push(minHeap.top());
+        minHeap.pop();
+       }
+       while(!maxHeap.empty() && res.size() < 10){
+        auto tweet_info = maxHeap.top();
+        res.push_back(tweet_info[1]);
+        maxHeap.pop();
+        int indx = tweet_info[3];
+        if(indx > 0){
+            auto tweet = tweet_map[tweet_info[2]][indx - 1];
+            maxHeap.emplace(std::vector{tweet.second, tweet.first, tweet_info[2], indx - 1});
+        }
+       }
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        follower_map[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        follower_map[followerId].erase(followeeId);
+    }
+ 
+};
