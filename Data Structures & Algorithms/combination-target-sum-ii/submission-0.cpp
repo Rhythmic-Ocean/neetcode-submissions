ea9c93e46584bdb0dd6dfa867a96cb2edb2f7d76
Samir Khanal <96647127+Rhythmic-Ocean@users.noreply.github.com>
2026-09-21 11:43:29 -0500

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    vector<vector<int>> finalAns {};
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+       set<int> undup {};
+       vector<int> sorted{};
+       for(auto candidate: candidates){
+        undup.insert(candidate);
+       } 
+       for(auto num: undup){
+        sorted.push_back(num);
+       }
+        std::ranges::sort(sorted);
+        std::ranges::sort(candidates);
+        vector<int> newVec {};
+        for(int i {}; i < candidates.size(); ++i){
+            if(i > 0 && candidates[i] == candidates[i - 1]) continue;
+            if(candidates[i] > target) break;
+            newVec.push_back(candidates[i]);
+            backtracking(candidates, target, candidates[i], i, newVec);
+            newVec.pop_back();
+        }
+        return finalAns;
+    }
+
+    void backtracking(vector<int>& candidates, int target, int sum, int indx, vector<int>& curVec){
+        if(sum == target){
+            finalAns.push_back(curVec);
+            return;
+        }
+        for(int i {indx+1}; i < candidates.size(); ++i){
+            if(i > indx+1 && candidates[i] == candidates[i - 1]) continue;
+            if(sum + candidates[i] > target) return;
+            curVec.push_back(candidates[i]);
+            backtracking(candidates, target, sum + candidates[i], i, curVec);
+            curVec.pop_back();
+        }
+        return;
+    }
+};
