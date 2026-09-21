267ad9fab9c380280c13bcc18ca8246fab6ba6bb
Samir Khanal <96647127+Rhythmic-Ocean@users.noreply.github.com>
2026-09-21 12:45:58 -0500

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<vector<int>> finalAns {};
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        set<int> exclude {};
+        int height = nums.size();
+       vector<int> new_vec {};
+       for(int i {}; i < nums.size(); ++i){
+            new_vec.push_back(nums[i]);
+            exclude.insert(i);
+            backtracking(nums, i, new_vec, exclude);
+            exclude.erase(i);
+            new_vec.pop_back();
+       } 
+       return finalAns;
+    }
+
+    void backtracking(vector<int>& nums, int indx, vector<int>& new_vec, set<int>& exclude){
+        if(nums.size() == exclude.size()) {
+            finalAns.push_back(new_vec);
+            return;
+        }
+        for(int i {}; i < nums.size(); ++i){
+            if(exclude.contains(i)) continue;
+            new_vec.push_back(nums[i]);
+            exclude.insert(i);
+            backtracking(nums, i, new_vec, exclude);
+            exclude.erase(i);
+            new_vec.pop_back();
+        } 
+    }
+};
